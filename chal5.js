8b1fb8c68e2b527516932c0d177d9f144da13054
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-11-07 12:45:50 -0600

    Add files via upload

---
+++ chal5.js
@@ -0,0 +1,21 @@
+//array of numbers from 0-100
+var myArr=[];
+    for(var i=0; i<=100; i++) {
+        myArr.push(i);
+}
+console.log(myArr);
+
+//array of multiplication of 5 from 0-100 
+
+var myArray=[];
+    for(var i=0; i<=100; i+=5) {
+        myArray.push(i);
+    }
+    console.log(myArray);
+
+//array of numbers from 0-100 in descending order
+var arr=[];
+for(i=100; i>=0; i-- ) {
+    arr.push(i);
+}
+console.log(arr);
